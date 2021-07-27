a85af018fd53803e8f7831f7f686a76f9b3cb79d
theforager <0xforager@gmail.com>
2021-07-26 23:32:47 -0500

    Implement Vendor contract

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,20 +1,43 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
+contract Vendor is Ownable {
 
   YourToken yourToken;
+  uint256 public constant tokensPerEth = 100;
+
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
 
   constructor(address tokenAddress) public {
     yourToken = YourToken(tokenAddress);
   }
 
-  //ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    uint256 newTokenAmount = msg.value * tokensPerEth;
+    emit BuyTokens(msg.sender, msg.value, newTokenAmount);
 
-  //ToDo: create a sellTokens() function:
+    yourToken.transfer(msg.sender, newTokenAmount);
+  }
 
-  //ToDo: create a withdraw() function that lets the owner, you can 
-  //use the Ownable.sol import above:
+  function sellTokens(uint256 tokenAmount) public {
+    address seller = msg.sender;
+    uint256 ethToReturn = tokenAmount / tokensPerEth;
+
+    emit SellTokens(seller, tokenAmount, ethToReturn);
+
+    yourToken.transferFrom(seller, address(this), tokenAmount);
+
+    (bool success, ) = seller.call{value: ethToReturn}("");
+    require(success, 'Token sale not successful');
+  }
+
+  function withdraw() public onlyOwner {
+    uint256 balance = yourToken.balanceOf(address(this));
+    yourToken.transfer(this.owner(), balance);
+    (bool success, ) = msg.sender.call{value: address(this).balance}("");
+    require(success, "Withdraw failed");
+  }
 }
